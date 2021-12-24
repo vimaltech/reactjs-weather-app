022ba8794d52292005ad6249d3b206b3d43e5e6a
DESKTOP-MUQ9LF5\vimal <vimal.tech@gmail.com>
2021-12-24 16:11:10 +0530

    Base react package

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Weather from './pages/weather';
+import './root.css';
+
+ReactDOM.render(<Weather />, document.getElementById('root'));
+
+
+// "build": "webpack",
+// "start": "webpack serve --open",
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
