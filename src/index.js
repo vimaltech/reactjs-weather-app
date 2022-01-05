9b770a71c89fc52085627b437e6afc51044aafd4
DESKTOP-MUQ9LF5\vimal <vimal.tech@gmail.com>
2022-01-05 09:30:28 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { App } from "./pages/app";
+import "./root.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// "build": "webpack",
+// "start": "webpack serve --open",
